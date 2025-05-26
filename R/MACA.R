7545bd5d24fba8789c4893f39b104b439d1529b4
Cathy Shen <cathy.shen@mail.mcgill.ca>
2025-05-26 15:56:55 -0400

    Fixed Indexing
    issues with rowind are fixed

--- R/MACA.R
+++ R/MACA.R
@@ -1,4 +1,4 @@
-#' @import coloc 
+#' @import coloc
 #' @import data.table
 
 #'@param t1 SuSiEx .snp file output from trait 1
@@ -11,40 +11,40 @@
 
 
 coloc_susiex <- function(t1, t2, p1 = 1e-4, p2=1e-4, p12=5e-6, overlap.min=0.5, trim_by_posterior=T) {
-  
+
   t1 <- as.data.frame(t1)
   t2 <- as.data.frame(t2)
-  
+
   ncst1 <- unique(gsub(".*\\((CS[0-9]+).*", "\\1", names(t1)[grep("LogBF\\(CS", names(t1))]))
   t1matrix <- matrix(ncol = nrow(t1), nrow = length(ncst1))
-  
+
   ncst2 <- unique(gsub(".*\\((CS[0-9]+).*", "\\1", names(t2)[grep("LogBF\\(CS", names(t2))]))
   t2matrix <- matrix(ncol = nrow(t2), nrow = length(ncst2))
-  
+
   if ((length(ncst1) ==0) | (length(ncst2) == 0)) {
     return(NA)
   }
-  
+
   for (k in 1:length(ncst1)) {
     cs_col <- grep(paste0(ncst1[k], ",Pop"), names(t1), value = TRUE)
     sumlogbf <- rowSums(t1[,which(colnames(t1)%in% cs_col)])
     t1matrix[k,] <- sumlogbf
   }
-  
+
   colnames(t1matrix) <- t1$SNP
-  
+
   for (k in 1:length(ncst2)) {
     cs_col <- grep(paste0(ncst2[k], ",Pop"), names(t2), value = TRUE)
     sumlogbf <- rowSums(t2[,which(colnames(t2)%in% cs_col)])
     t2matrix[k,] <- sumlogbf
   }
-  
+
   colnames(t2matrix) <- t2$SNP
-  
+
   coloc_susiex_result <- coloc:::coloc.bf_bf(t1matrix, t2matrix, p1, p2, p12, overlap.min, trim_by_posterior)
-  
+
   return(coloc_susiex_result)
-  
+
 }
 
 #'@param t1 mscaviar post.txt output file for trait 1
@@ -57,10 +57,10 @@ msecaviar <- function(t1,t2) {
   scaled_clpp <- clpp / sum(clpp)
   locus_level_clpp <- sum(clpp)
   snps <- t1$SNP_ID
-  
+
   msecaviar_result <- list(data.frame(snps, scaled_clpp, clpp), locus_level_clpp)
   names(msecaviar_result) <- c("variant_level", "locus_level_clpp")
-  
+
   return(msecaviar_result)
 }
 
@@ -70,50 +70,50 @@ msecaviar <- function(t1,t2) {
 #'@export
 
 susiex_ecaviar <- function(t1, t2) {
-  
+
   t1 <- as.data.frame(t1)
   t2 <- as.data.frame(t2)
-  
+
   pip_cols1 <- grep("PIP", names(t1), value = TRUE)
   pip_cols2 <- grep("PIP", names(t2), value = TRUE)
-  
+
   if ((length(pip_cols1) == 0) | (length(pip_cols2) == 0)) { return(NA)}
-  
+
   t1pip <- as.matrix(t1[, which(colnames(t1) %in% pip_cols1)])
   colnames(t1pip) <- pip_cols1
   t2pip <- as.matrix(t2[, which(colnames(t2) %in% pip_cols2)])
   colnames(t2pip) <- pip_cols2
-  
+
   # t11pip <- t11[,..pip_cols1]
   # t22pip <- t22[,..pip_cols2]
-  
+
   coloc_comparisons <- expand.grid(colnames(t1pip), colnames(t2pip), stringsAsFactors = FALSE)
   # clpp_list <- mapply(function(col1, col2) {
   #   t11pip[[col1]] * t22pip[[col2]]
   #  }, coloc_comparisons[,1], coloc_comparisons[,2], SIMPLIFY = FALSE)
 
-  
+
   clpp_list <- mapply(function(col1, col2) {
     t1pip[,which(colnames(t1pip) == col1)] * t2pip[,which(colnames(t2pip) == col2)]
   }, coloc_comparisons[,1], coloc_comparisons[,2], SIMPLIFY = FALSE)
-  
-  names(clpp_list) <- paste(sub(".*\\((.*)\\).*", "\\1", coloc_comparisons[,1]), 
+
+  names(clpp_list) <- paste(sub(".*\\((.*)\\).*", "\\1", coloc_comparisons[,1]),
                             "x", sub(".*\\((.*)\\).*", "\\1", coloc_comparisons[,2]), sep = "_")
-  
+
   clpp <- (as.data.frame(clpp_list))
   scaled_clpp <- sweep(clpp, 2, colSums(clpp), FUN = "/")
   locus_level_clpp <- colSums(clpp)
-  
+
   clpp$snps <- t1$SNP
   scaled_clpp$snps <- t1$SNP
-  
-  
+
+
   results <- list(clpp, scaled_clpp, locus_level_clpp)
   names(results) <- c("clpp", "scaled_clpp", "locus_level_clpp")
-  
-  
+
+
   return(results)
-  
+
 }
 
 #'@param t1 MsCAVIAR post.txt output from trait 1
@@ -125,37 +125,37 @@ susiex_ecaviar <- function(t1, t2) {
 #'@export
 
 mscaviar_coloc <- function(t1, t2, p1 = 1e-4, p2=1e-4, p12=5e-6, overlap.min=0.5, trim_by_posterior=T) {
-  
+
   t1$Prob_in_pCausalSet[which(t1$Prob_in_pCausalSet == 1)] <- 1 - 1e-12
   t1$Prob_in_pCausalSet[which(t1$Prob_in_pCausalSet == 0)] <- 1e-12
   t2$Prob_in_pCausalSet[which(t2$Prob_in_pCausalSet == 1)] <- 1 - 1e-12
   t2$Prob_in_pCausalSet[which(t2$Prob_in_pCausalSet == 0)] <- 1e-12
-  
+
   bft1 <- t1$Prob_in_pCausalSet/(1-t1$Prob_in_pCausalSet) * (1-p1)/p1
   logbft1 <- as.vector(unlist(log(bft1)))
   bft2 <- t2$Prob_in_pCausalSet/(1-t2$Prob_in_pCausalSet) * (1-p2)/p2
   logbft2 <- as.vector(unlist(log(bft2)))
   names(logbft1) <- names(logbft2) <- as.vector(unlist(t1$SNP_ID))
-  
+
   mscaviar_coloc_result <- coloc:::coloc.bf_bf(logbft1, logbft2, p1, p2, p12, overlap.min, trim_by_posterior)
   return(mscaviar_coloc_result)
-  
+
 }
 
 
-#'@param cs_res coloc_SuSiEx result 
+#'@param cs_res coloc_SuSiEx result
 #'@param rowind coloc comparison of interest to generate a credible set for (which row)
 #'@export
 
 make_coloc_susiex_credible_set <- function(cs_res, rowind) {
   cs_res$results <- as.data.frame(cs_res$results)
-  cs_res_sorted <- cs_res$results[order(cs_res$results[,rowind], decreasing = TRUE),]
-  cumsum <- cumsum(cs_res_sorted[,rowind])
-  if (cs_res_sorted[1,rowind] > 0.95) {
+  cs_res_sorted <- cs_res$results[order(cs_res$results[,rowind+1], decreasing = TRUE),]
+  cumsum <- cumsum(cs_res_sorted[,rowind+1])
+  if (cs_res_sorted[1,rowind+1] > 0.95) {
     cs <- cs_res_sorted[1,]
   } else {
     cs <- cs_res_sorted[which(cumsum <= 0.95),]
-    while (cumsum(cs[,rowind])[nrow(cs)] < 0.95) {
+    while (cumsum(cs[,rowind+1])[nrow(cs)] < 0.95) {
       cs <- rbind(cs, cs_res_sorted[nrow(cs)+1,])
     }
   }
@@ -176,7 +176,7 @@ make_msecaviar_credible_set <- function(mse_res) {
       cs <- rbind(cs, mse_res_sorted[nrow(cs)+1,])
     }
   }
-  
+
   return(cs)
 }
 
@@ -185,13 +185,13 @@ make_msecaviar_credible_set <- function(mse_res) {
 #'@param rowind coloc comparison to generate a credible set for (which row)
 #'@export
 make_susiex_ecaviar_credible_set <- function(se_res, rowind) {
-  se_res_sorted <- se_res[order(se_res[,rowind], decreasing = TRUE),]
-  cumsum <- cumsum(se_res_sorted[,rowind]) 
-  if (se_res_sorted[1,rowind] > 0.95) {
+  se_res_sorted <- se_res[order(se_res[,rowind+1], decreasing = TRUE),]
+  cumsum <- cumsum(se_res_sorted[,rowind+1])
+  if (se_res_sorted[1,rowind+1] > 0.95) {
     cs <- se_res_sorted[1,]
   } else {
     cs <- se_res_sorted[which(cumsum <= 0.95),]
-    while (cumsum(cs[,rowind])[nrow(cs)] < 0.95) {
+    while (cumsum(cs[,rowind+1])[nrow(cs)] < 0.95) {
       cs <- rbind(cs, se_res_sorted[nrow(cs) + 1,])
     }
   }
